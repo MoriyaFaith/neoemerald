3e6b700cf03cef4659804dea950085bd9eaffcf9
sophiap <sophiap@sophia-nitroan51554>
2023-02-08 11:46:50 -0500

    Faithful Flare Blitz

--- src/data/battle_moves_faithful.h
+++ src/data/battle_moves_faithful.h
@@ -4615,6 +4615,19 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
         .flags = FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGS_ROCK_AFFECTED,
     },
 
+    [MOVE_FLARE_BLITZ] =
+    {
+        .effect = EFFECT_DOUBLE_EDGE,
+        .power = 120,
+        .type = TYPE_FIRE,
+        .accuracy = 100,
+        .pp = 15,
+        .secondaryEffectChance = 0,
+        .target = MOVE_TARGET_SELECTED,
+        .priority = 0,
+        .flags = FLAG_MAKES_CONTACT | FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGS_ROCK_AFFECTED,
+    },
+
     [MOVE_X_SCISSOR] =
     {
         .effect = EFFECT_HIT,
