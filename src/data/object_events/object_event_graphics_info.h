48164c8cb0a9322edc321cb9457eab447f5dcacc
MoriyaFaith <jcp1609@outlook.com>
2023-04-21 16:51:16 -0400

    fix wally's color

--- src/data/object_events/object_event_graphics_info.h
+++ src/data/object_events/object_event_graphics_info.h
@@ -2584,7 +2584,7 @@ const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_Steven = {
 
 const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_Wally = {
     .tileTag = TAG_NONE,
-    .paletteTag = OBJ_EVENT_PAL_TAG_NPC_1,
+    .paletteTag = OBJ_EVENT_PAL_TAG_WALLY,
     .textColor = NPC_TEXT_COLOR_MALE,
     .size = 256,
     .width = 16,
