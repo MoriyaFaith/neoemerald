8e446d0e8c419aec53108becb74fbb0cbd2e18b5
MoriyaFaith <jcp1609@outlook.com>
2023-05-08 14:34:41 -0400

    fixed sell menu displaying wrong money amount

--- src/item_menu.c
+++ src/item_menu.c
@@ -1161,8 +1161,8 @@ static void PrintItemSoldAmount(int windowId, int numSold, int moneyEarned)
     u8 numDigits = (gBagPosition.pocket == BERRIES_POCKET) ? BERRY_CAPACITY_DIGITS : BAG_ITEM_CAPACITY_DIGITS;
     ConvertIntToDecimalStringN(gStringVar1, numSold, STR_CONV_MODE_LEADING_ZEROS, numDigits);
     StringExpandPlaceholders(gStringVar4, gText_xVar1);
-    AddTextPrinterParameterized(windowId, FONT_NORMAL, gStringVar4, 0, 1, TEXT_SKIP_DRAW, 0);
-    PrintMoneyAmount(windowId, 38, 1, moneyEarned, 0);
+    AddTextPrinterParameterized(windowId, FONT_NORMAL, gStringVar4, 2, 10, TEXT_SKIP_DRAW, 0);
+    PrintMoneyAmount(windowId, 54, 10, moneyEarned, 0);
 }
 
 static void Task_BagMenu_HandleInput(u8 taskId)
